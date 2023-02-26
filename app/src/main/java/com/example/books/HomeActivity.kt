3e96e929818e89cd3c41e1898a4c2cc1d8f51d51
Julian Muñoz <julianmuniz753@gmail.com>
2023-02-26 17:43:43 -0300

    Agregamos el adapter al Recycler View

--- app/src/main/java/com/example/books/HomeActivity.kt
+++ app/src/main/java/com/example/books/HomeActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
-private lateinit var ToolbarBooks:Toolbar
+private lateinit var ToolbarBooks: Toolbar
+private lateinit var RecyclerViewBooks:RecyclerView
+private var listaLibros = mutableListOf<Book>(
+    Book("Harry Potter","1999","Jk Rowling")
+)
 
 
 class HomeActivity : AppCompatActivity() {
@@ -19,10 +25,27 @@ class HomeActivity : AppCompatActivity() {
 
         //Funcion Para Modificar el toolbar
         SetupToolbar()
+
+        //Funcion para Modificar el Recycler View
+        SetupRecyclerView()
     }
 
     private fun SetupToolbar() {
 
         setSupportActionBar(ToolbarBooks)
     }
+
+    private fun SetupRecyclerView() {
+
+        RecyclerViewBooks = findViewById(R.id.recyclerviewbooks)
+        RecyclerViewBooks.setHasFixedSize(true)
+        RecyclerViewBooks.layoutManager = LinearLayoutManager(this)
+
+        val adapter = BooksAdapter(listaLibros)
+        RecyclerViewBooks.adapter = adapter
+
+
+
+    }
+
 }
