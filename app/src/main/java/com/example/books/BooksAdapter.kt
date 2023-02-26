7360a3f56ec9e5df15738ff54d4efd9ee0984b15
Julian Muñoz <julianmuniz753@gmail.com>
2023-02-26 17:39:28 -0300

    Creamos el Adapter

---
+++ app/src/main/java/com/example/books/BooksAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.books
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksAdapter(val Libros: List<Book>) : RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_book, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BooksAdapter.ViewHolder, position: Int) {
+        val item = Libros.get(position)
+        holder.render(item)
+
+    }
+
+    override fun getItemCount(): Int {
+        return Libros.size
+    }
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        val NombreTextView: TextView = view.findViewById(R.id.titulo)
+        val AñoTextView: TextView = view.findViewById(R.id.año)
+        val AutorTextView: TextView = view.findViewById(R.id.autor)
+
+
+        fun render(libro: Book) {
+            NombreTextView.text = libro.Titulo
+            AñoTextView.text = libro.Año
+            AutorTextView.text = libro.Autor
+
+        }
+
+    }
+
+}
