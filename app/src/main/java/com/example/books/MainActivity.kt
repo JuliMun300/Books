37d2eabb664f4d60e0a3900210fd7e5ea7f0cc4a
Julian Muñoz <julianmuniz753@gmail.com>
2023-05-08 17:30:48 -0300

    Agregamos y aplicamos la Auntenticacion de usuarios de Firebase

--- app/src/main/java/com/example/books/MainActivity.kt
+++ app/src/main/java/com/example/books/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.books
 
-import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 private lateinit var TextoEmail: TextView
 private lateinit var TextoContraseña: TextView
 private lateinit var BotonIngresar: Button
+private lateinit var BotonCrearUsuario: Button
 private lateinit var CheckboxGuardar: CheckBox
 private lateinit var Prefs: SharedPreferences
 
@@ -26,13 +29,17 @@ class MainActivity : AppCompatActivity() {
         TextoContraseña = findViewById(R.id.ContraseñaTextField)
         BotonIngresar = findViewById(R.id.botonIngresar)
         CheckboxGuardar = findViewById(R.id.checkBoxguardar)
+        BotonCrearUsuario = findViewById(R.id.botonCrearUsuario)
         Prefs = getSharedPreferences("Preferencias", MODE_PRIVATE)
-
-        CargarDatos()
 
         //Agregamos el boton y cuando se presione se validan los campos
         BotonIngresar.setOnClickListener {
             ValidarCampos()
+        }
+
+        //Agregamos el boton cuando se presione para ejecutar el registro del usuario
+        BotonCrearUsuario.setOnClickListener {
+            RegistroCampos()
         }
     }
 
@@ -41,17 +48,40 @@ class MainActivity : AppCompatActivity() {
         if (TextoEmail.text.toString().isEmpty() || TextoContraseña.text.toString().isEmpty()) {
             Toast.makeText(this, "Ingresa los campos", Toast.LENGTH_SHORT).show()
         } else {
-            startActivity(Intent(this, HomeActivity::class.java))
-            finish()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                TextoEmail.text.toString(),
+                TextoContraseña.text.toString()
+            ).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    finish()
+                } else {
+                    ShowAlert()
+                }
+            }
         }
+    }
 
-        if (CheckboxGuardar.isChecked) {
-            GuardarDatos(TextoEmail.text.toString(), TextoContraseña.text.toString())
+    //Funcion para registrar el usuario
+    private fun RegistroCampos() {
+        if (TextoEmail.text.toString().isEmpty() || TextoContraseña.text.toString().isEmpty()) {
+            ShowAlert()
+        } else {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                TextoEmail.text.toString(),
+                TextoContraseña.text.toString()
+            ).addOnCompleteListener { task ->
+                if (task.isComplete) {
+                    Toast.makeText(this, "Usuario Creado", Toast.LENGTH_SHORT).show()
+                } else {
+                    ShowAlert()
+                }
+            }
         }
     }
 
     //Funcion para guardar datos cada vez que se el usuario presione Recordad Usuario
-    private fun GuardarDatos(nombre: String, Pass: String) {
+    fun GuardarDatos(nombre: String, Pass: String) {
         val editor = Prefs.edit()
         editor.putString("nombre", nombre)
         editor.putString("pass", Pass)
@@ -74,5 +104,13 @@ class MainActivity : AppCompatActivity() {
                 TextoContraseña.text = contraseñaGuardada
             }
         }
+    }
+
+    //Funcion para mostrar un mensaje de error
+    fun ShowAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("A ocurrido un error")
+        builder.show()
     }
 }
